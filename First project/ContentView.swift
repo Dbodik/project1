cd64d645e50698e5082e690f5aae51a36b2dba96
Dbodik <bogdan.dashkin@yandex.ru>
2022-04-12 17:43:58 +0300

    Initial Commit

---
+++ First project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First project
+//
+//  Created by Богдан Дашкин on 12.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First project/First_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_projectApp.swift
+//  First project
+//
+//  Created by Богдан Дашкин on 12.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct First_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
